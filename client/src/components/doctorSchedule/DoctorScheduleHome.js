df0d8e7b1e910351c17775eebfe155216cc5175d
DESKTOP-C2OTNLH\Uthpala <uthpalaisuranga990@gmail.com>
2022-09-07 20:47:01 +0530

    Frontend-View Doctor's Schedule by Doctor - p2

--- client/src/components/doctorSchedule/DoctorScheduleHome.js
+++ client/src/components/doctorSchedule/DoctorScheduleHome.js
@@ -6,8 +6,10 @@ import { GlobalState } from "../../GlobalState";
 function DoctorScheduleHome() {
   const state = useContext(GlobalState)
   const [token] = state.token
+  const [userDetails] = state.userAPI.userDetails
   const [doctorSchedules, setDoctorSchedules] = useState([]);
   const [callback, setCallback] = useState(true);
+  const navigate = useNavigate()
 
   useEffect(() => {
     if (callback) {
@@ -42,43 +44,91 @@ function DoctorScheduleHome() {
     setCallback(true)
   }
 
-  const navigate = useNavigate()
-
   const editDoctorSchedule = (doctorScheduleId) => {
     navigate(`/doctor/editADoctorSchedule/${doctorScheduleId}`)
   }
 
+  const addNewVisit = () => {
+    navigate('/doctor/addDoctorsSchedule');
+  }
+
   return (
-    <div>
+    <div className="doctorScheduleMainLayout">
+      <br /><br />
 
+      {/* <div className="container">
+        <div className="row">
+          <div className="col-sm-3"><center><img className='pLogo' src={userDetails.logo} style={{ width : "200px"  , height : "200px"}}/></center></div>
+          <div className="col-sm-6">
+            <div className="container" style={{ fontWeight: "400", fontSize: "18px" }}>
+              <div className="row m-2">
+                <div className="row" style={{ fontWeight: "500", fontSize: "25px" }}>
+                  Dr. {userDetails.name}
+                </div>
+                <div className="row">
+                  {userDetails.doctor.speciality} 
+                </div>
+                <br/>
+                <div className="row m-4">
+                  <div className="col-8">{userDetails.doctor.phone}</div>
+                  <div className="col-4">{userDetails.doctor.service} Years service</div>
+                </div>
+                <div className="row ms-4">
+                  <div className="col-3">{userDetails.email}</div>
+                  <div className="col-3"></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div> */}
 
-      <br />
-      <br />
-
-      <Link to="/doctor/addDoctorsSchedule">Add New Visit</Link>
-
-      <div>
-        <table>
-          <thead>
-            <tr>
-              <th>Product Code</th>
-              <th>Product Name</th>
-              <th>Action</th>
-              <th>Action</th>
-            </tr>
-          </thead>
-          <tbody>
-            {doctorSchedules.map(doctorSchedule => (
-              <tr key={doctorSchedule.id}>
-                <td>{doctorSchedule.day}</td>
-                <td>{doctorSchedule.startTime}</td>
-                <td>{doctorSchedule.endTime}</td>
-                <td>{doctorSchedule.location}</td>
-                <td><button onClick={() => editDoctorSchedule(doctorSchedule._id)}>Edit</button><button onClick={() => deleteDoctorSchedule(doctorSchedule._id)}>Delete</button></td>
-              </tr>))}
-          </tbody>
-        </table>
+      <br/><br/>
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-9"><center><h2 className="" style={{ fontWeight: "bolder", fontSize: "30px" }}>My Visiting Schedule</h2></center></div>
+          <div class="col-sm-3"><button className="addVisitBtn" onClick={() => addNewVisit()} style={{ width: "180px" }} >Add New Visit</button></div>
+        </div>
       </div>
+
+
+
+      <br />
+
+
+      {doctorSchedules.map(doctorSchedule => (
+        <div className="parent container d-flex justify-content-center align-items-center h-100">
+          <div className="doctorScheduleLayout" >
+            <div class="container">
+              <div class="row">
+                <div class="col-sm-9 ">
+                  <div class="">
+                    <div class="row">
+                      <div class="col">
+                        {doctorSchedule.day}
+                      </div>
+                      <div class="col">
+                        {doctorSchedule.startTime} - {doctorSchedule.endTime}
+                      </div>
+                      <div class="col">
+                        {doctorSchedule.location}
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-sm-3 ">
+                  <div class="row">
+                    <button className="editBtn" onClick={() => editDoctorSchedule(doctorSchedule._id)}>Edit</button>
+                  </div>
+                  <div class="row">
+                    <button className="deleteBtn" onClick={() => deleteDoctorSchedule(doctorSchedule._id)}>Delete</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      ))}
     </div>
   );
 
