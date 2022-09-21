196381dce712d2ad4c37d398582804e01c7a803b
amila1998 <amiladevin@gmail.com>
2022-09-22 02:23:44 +0530

    Frontend-Rectrive Replies

---
+++ client/src/components/questions/replyCard/ReplyCard.js
@@ -0,0 +1,78 @@
+import React, { useContext, useEffect, useState } from 'react'
+import { GlobalState } from '../../../GlobalState'
+import './replyCard.css'
+import correcti from '../../../asserts/icons/correct.png';
+import deletei from '../../../asserts/icons/delete.png';
+import editi from '../../../asserts/icons/edit.png';
+import replyi from '../../../asserts/icons/reply.png';
+import sharei from '../../../asserts/icons/share.png';
+import wrongi from '../../../asserts/icons/wrong.png';
+import reporti from '../../../asserts/icons/report.png';
+import collespani from '../../../asserts/icons/collespan.png';
+import expandi from '../../../asserts/icons/expand.png';
+
+const ReplyCard = ({ reply }) => {
+    console.log("🚀 ~ file: ReplyCard.js ~ line 5 ~ ReplyCard ~ reply", reply)
+    const state = useContext(GlobalState)
+    const [userDetails] = state.userAPI.userDetails
+    const [myReply, setMyReply] = useState(false)
+    const [replilesHide, setReplyHide] = useState(false)
+
+    useEffect(() => {
+        if (userDetails) {
+            if (userDetails._id === reply.userID) {
+                setMyReply(true)
+            }
+        }
+    }, [userDetails, reply])
+
+
+    return (
+        <div className='rBody'>
+            <div className='rFrom'>
+                <h5>From : </h5>
+                <div className='rName'>{myReply ? 'You' : reply.userName}</div>
+            </div>
+            <div className='rMsg'>
+                {reply.replyBody}
+                <br />  <br />
+                <div className='rActions'>
+                    {
+                        myReply ?
+                            <>
+                                <div className='mar edit fW cMpointer' ><img className='im' src={editi} />Edit</div>
+                                <div className='mar report fW cMpointer' ><img className='im' src={deletei} />Delete</div></>
+                            :
+                            <>
+                                <div className='mar reply fW cMpointer' ><img className='im' src={replyi} />Reply</div>
+                                <div className='mar correct fW cMpointer' ><img className='im' src={correcti} />Correct</div>
+                                <div className='mar report fW cMpointer' ><img className='im' src={wrongi} />Wrong</div>
+                            </>
+                    }
+
+
+
+                </div>
+                <br />
+                <hr />
+            </div>
+            <div className='rCW'>
+                {reply.voteToCorrect.length} users said this answer is correct and  {reply.voteToWrong.length} users is said this is wrong answer
+                <br />
+
+            </div>
+            <hr />
+            <div className='rRCount' onClick={() => { reply.childReplies.length > 0 && setReplyHide(!replilesHide) }}>
+                Replies ({reply.childReplies.length}){reply.childReplies.length > 0 && <div className='imDiv'> {replilesHide ? <><img className='im2' src={expandi} /></> : <><img className='im3' src={collespani} /></>}</div>}
+            </div>
+            {
+                replilesHide && <div className='rReplies'>
+                    <hr /><hr />
+                </div>
+            }
+            <hr />
+        </div>
+    )
+}
+
+export default ReplyCard
