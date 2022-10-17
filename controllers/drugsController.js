753c9051f7cd1b51c5f52ce159be7f0e5b056038
sean-udayantha <seanudayantha@mail.com>
2022-10-17 16:34:54 +0530

    change drug back end

--- controllers/drugsController.js
+++ controllers/drugsController.js
@@ -34,6 +34,61 @@ const drugsController={
             }); 
         }
     },
+    getOne:async(req,res)=>{
+        try {
+            const pharmacyDrug = await drugs.find({"PharmacyID":req.params.id});
+            res.status(200).json({ 
+                pharmacyDrug,
+                success: true
+            })                
+        } catch (error) {
+            res.status(500).json({ 
+                msg: error.message ,
+                success: false
+            }); 
+        }
+    },
+    editDrug : async ( req , res ) => {
+        try {
+
+            let DrugId = req.params.id ;
+            const { DrugName , DrugQuantity, Description , DrugPrice , DrugImage } = req.body;
+
+            const newEditDrug = {
+                DrugName , DrugQuantity, Description , DrugPrice , DrugImage
+            };
+
+            const fetch = await drugs.findByIdAndUpdate(DrugId,newEditDrug);
+
+            res.status(200).json({ 
+                msg: "Update success." ,
+                fetch : fetch    
+            });
+
+        } catch (error) {
+            res.status(500).json({
+                msg: error.message,
+                success: false
+            });
+        }
+    },
+    deleteDrug : async (req, res) => {
+        try {
+            const fetch = await drugs.findByIdAndDelete(req.params.id);
+
+
+            res.status(200).json({
+                msg: "Drug Deleted Success ! ",
+                success: true,
+            });
+
+        } catch (error) {
+            res.status(500).json({
+                msg: error.message,
+                success: false
+            });
+        }
+    }
     
 
 }
